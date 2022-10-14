991256160626f5fda26023f233b20cbb216dea17
emon <imrannaaziremon@gmail.com>
2022-10-14 18:51:17 +0600

    customize theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 // 1. import `NextUIProvider` component
-import { NextUIProvider } from "@nextui-org/react";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    type: "dark",
+    theme: {
+      colors: {
+        emonColor: "$pink500",
+      },
+    },
+  });
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Avatar, Button } from "@nextui-org/react";
+import { Avatar, Button, Text } from "@nextui-org/react";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +9,7 @@ const Home: NextPage = () => {
         src="https://i.pravatar.cc/150?u=a04258114e29026702d"
         css={{ size: "$20" }}
       />
+      <Text css={{ color: "$emonColor" }}>Hello world</Text>
     </div>
   );
 };
